8353e699bfea79fa0343254b1254090cf2505e08
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-09-18 14:46:21 +0300

    email config complete.

---
+++ app/instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY = 'gf5rgtbf65gtbf63g6er65gzr'
+SECRET_KEY = '<Flask WTF Secret Key>'
---
+++ start.sh
@@ -0,0 +1,7 @@
+export MOVIE_API_KEY=<Your api key>
+export SECRET_KEY=<Your secret key>
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+
+
+python3.6 manage.py server
